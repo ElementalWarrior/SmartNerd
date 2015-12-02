730dd6e244f89d047e3992e09d14d33456b62ef6
Kelsey Maes <kelseymaes@outlook.com>
2015-12-01 19:07:39 -0800

    Give administrators their own section of the web site

--- SmartNerd/Controllers/AdminController.cs
+++ SmartNerd/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SmartNerd.Models.Admin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,68 @@ namespace SmartNerd.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public ActionResult Browse(int? categoryID = null)
+        {
+            SmartNerdDataContext dc = new SmartNerdDataContext();
+            List<Product> prods;
+            //get all products
+            if(categoryID == null)
+            {
+                prods = (from p in dc.Products
+                           select new Product
+                           {
+                               ProductName = p.Name,
+                               ProductID = p.ProductID,
+                               Price = p.Price,
+                               Description = p.Description
+                           }).ToList();
+            } else {
+                prods = (from c in dc.Categories
+                       join ce in dc.CategoryEntries on c.CategoryID equals ce.CategoryID
+                       join p in dc.Products on ce.ProductID equals p.ProductID
+                       where c.CategoryID == categoryID.Value
+                       select new Product
+                        {
+                            ProductName = p.Name,
+                            ProductID = p.ProductID,
+                            Price = p.Price,
+                            Description = p.Description
+                        }).ToList();
+            }
+            BrowsePage mp = new BrowsePage {
+                Categories = (from c in dc.Categories
+                            select new Category
+                            {
+                                CategoryID = c.CategoryID,
+                                CategoryName = c.Name
+                            }).ToList(),
+                Products = prods
+            };
+            return View(mp);
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public ActionResult Search(SearchPage model) {
+            SmartNerdDataContext dc = new SmartNerdDataContext();
+
+            List<Product> products = null;
+
+            if(model.SearchTerm != null) {
+                products = (from a in dc.Products
+                            where a.Name.Contains(model.SearchTerm)
+                            select new Product {
+                                Description = a.Description,
+                                Price = a.Price,
+                                ProductID = a.ProductID,
+                                ProductName = a.Name }).ToList();
+            }
+
+            model.Products = products;
+
+            return View(model);
         }
     }
 }
