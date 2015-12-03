6a1c88b85ea284edfd9c1ce2aa136a3b59ce51b2
Kelsey Maes <kelseymaes@outlook.com>
2015-12-03 10:49:51 -0800

    Add recovery function

--- SmartNerd/Controllers/HomeController.cs
+++ SmartNerd/Controllers/HomeController.cs
@@ -4,24 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace SmartNerd.Controllers
-{
-    public class HomeController : BaseController
-    {
-        public ActionResult Index()
-        {
-            return RedirectToAction("Index", "Menu");
+namespace SmartNerd.Controllers {
+    public class HomeController: BaseController {
+        public ActionResult Index() {
+            if(User.IsInRole("Administrator")) return RedirectToAction("Browse","Admin");
+            return RedirectToAction("Index","Menu");
         }
 
-        public ActionResult About()
-        {
+        public ActionResult About() {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
-        {
+        public ActionResult Contact() {
             ViewBag.Message = "Your contact page.";
 
             return View();
